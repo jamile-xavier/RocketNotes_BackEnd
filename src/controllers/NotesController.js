2ea5dba2ccaf1973a278e2169220589f807ba65b
Jamile-Xavier <jamile.x.m@gmail.com>
2023-07-31 11:29:32 -0300

    atualizações integração back e front

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -70,6 +70,7 @@ class NotesController {
         .whereLike("notes.title", `%${title}%`)
         .whereIn("name", filterTags)
         .innerJoin("notes", "notes.id", "tags.note_id")
+        .groupBy("notes.id")
         .orderBy("movie_notes.title");
     } else {
       notes = await knex("notes")
