6526a4172821b1191ae3b908ff7eb952bc7bd16b
Jamile-Xavier <jamile.x.m@gmail.com>
2023-07-31 20:01:33 -0300

    Configurações deploy

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require("dotenv/config");
 require("express-async-errors");
 const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
@@ -31,5 +32,5 @@ app.use((error, request, response, next) => {
   });
 });
 
-const PORT = 3333;
+const PORT = process.env.SERVER_PORT || 3333;
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
